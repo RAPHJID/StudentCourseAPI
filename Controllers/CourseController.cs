9b84c4ac6dfc7e206a11a364b914c295fbc5bb4f
jidraf <Kimachiajidraf@gmail.com>
2025-06-25 22:19:43 +0300

    Child Resource : CourseController Created

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentCourseAPI.Models;
+using StudentCourseAPI.Services.IServices;
+
+namespace StudentCourseAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class CourseController : ControllerBase
+{
+    public readonly ICourseService _courseService;
+
+    public CourseController(ICourseService courseService)
+    {
+        _courseService = courseService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllCourses()
+    {
+        return Ok(_courseService.GetAllCourses());
+    }
+
+    [HttpGet("{courseId}")]
+    public IActionResult GetCourseById(int courseId)
+    {
+        var course = _courseService.GetCourseById(courseId);
+        if(course == null) return NotFound();
+        return OK(course);
+    }
+
+    [HttpPost]
+    public IActionResult AddCourse(Course newCourse)
+    {
+        _courseService.AddCourse(newCourse);
+        return Ok(newCourse);
+    }
+
+    [HttpPut("{courseId}")]
+    public IActionResult UpdateCourse(int courseId, Course updatedCourse)
+    {
+        var existing = _courseService.GetCourseById(courseId);
+        if(existing == null) return NotFound();
+        _courseService.UpdateCourse(courseId, updatedCourse);
+        return Ok(updatedCourse);
+    }
+
+    [HttpDelete("{courseId}")]
+    public IActionResult DeleteCourse(int courseId)
+    {
+        var course = _courseService.GetCourseById(courseId);
+        if(course == null) return NotFound();
+
+        _courseService.DeleteCourse(courseId);
+        return Ok($"Course with ID {courseId} Deleted!!");
+    }
+
+
+    
+}
